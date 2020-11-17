fbdd5e8cfff71283bf1cfef7a84fb491397f959b
White Hamtaro <mattanguyen31@gmail.com>
2020-11-16 22:15:42 -0800

    Simplify: just split train/test right away

--- work2vec/main.py
+++ work2vec/main.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import random
+from sklearn.ensemble import ExtraTreesClassifier
 
 import pandas as pd
 
@@ -28,56 +29,57 @@ def clean_text_noise(tt: str):
 # Exmaple train file: http://magnitude.plasticity.ai/data/atis/atis-intent-train.txt
 # Exmaple test file : http://magnitude.plasticity.ai/data/atis/atis-intent-test.txt
 def format_for_glove(text: str, target: str):
-    return "BOS {} EOS {}\n".format(text, target)
+    return "BOS {} EOS {}".format(text, target)
 
 
-def prepare_clean_corpus(data_dir: str, output_txt: str):
+def prepare_train_test(data_dir: str, train_ff: str, test_ff: str):
     ffs = [f for f in listdir(data_dir) if isfile(join(data_dir, f)) and f.endswith('.csv')]
-    with open(output_txt, 'w') as fw:
-        for ff in ffs:
-            ff_path = join(data_dir, ff)
-            #print(ff_path)
-            if ff_path.endswith('_D.csv'):
-                target = TAR_DEMOCRAT
-            elif ff_path.endswith('_R.csv'):
-                target = TAR_REPUBLIC
-            else:
-                raise Exception("Error reading {}!".format(ff_path))
-            df = pd.read_csv(join(data_dir, ff))
-            titles = df['title'].to_list()
-            for title in titles:
-                strline = clean_text_noise(title)
-                strline = format_for_glove(strline, target)
-                #print(to_write)
-                fw.write(strline)
-    pass
+    democrats = []
+    republics = []
+    for ff in ffs:
+        ff_path = join(data_dir, ff)
+        #print(ff_path)
+        if ff_path.endswith('_D.csv'):
+            target = TAR_DEMOCRAT
+        elif ff_path.endswith('_R.csv'):
+            target = TAR_REPUBLIC
+        else:
+            raise Exception("Error reading {}!".format(ff_path))
+        df = pd.read_csv(join(data_dir, ff))
+        titles = df['title'].to_list()
+        selected_group = democrats if target == TAR_DEMOCRAT else republics
+        for title in titles:
+            strline = clean_text_noise(title)
+            strline = format_for_glove(strline, target)
+            selected_group.append(strline)
 
-
-def train_test_split(corpus_ff: str, train_ff: str, train_ratio: float, test_ff: str):
-    with open(corpus_ff, mode='rb') as f:
-        lines = f.readlines()
-    lines = [l.decode('utf-8').strip() for l in lines]
-    #print(lines[-1])
-    #print(len(lines))
-    random.shuffle(lines)
-    cutoff = int(len(lines) * train_ratio)
+    random.shuffle(democrats)
+    random.shuffle(republics)
+    print("democrats =", len(democrats))
+    print("republics =", len(republics))
+    c1 = int(len(democrats) * TRAIN_RATIO)
+    c2 = int(len(republics) * TRAIN_RATIO)
+    trains = democrats[:c1] + republics[:c2]
+    tests = democrats[c1:] + republics[c2:]
+    random.shuffle(trains)
+    random.shuffle(tests)
+    print("trains =", len(trains))
+    print("tests =", len(tests))
     with open(train_ff, 'w') as ftrain:
-        for ll in lines[:cutoff]:
+        for ll in trains:
             ftrain.write(ll+"\n")
     with open(test_ff, 'w') as ftest:
-        for ll in lines[cutoff:]:
+        for ll in tests:
             ftest.write(ll+"\n")
     pass
 
 
 def main():
     data_dir = './rawdata'
-    corpus_ff = './corpus.txt'
     train_ff = './train.txt'
     test_ff = './test.txt'
-    #prepare_clean_corpus(data_dir, corpus_ff)
+    prepare_train_test(data_dir, train_ff, test_ff)
 
-    #train_test_split(corpus_ff, train_ff, TRAIN_RATIO, test_ff)
     pass
 
 
